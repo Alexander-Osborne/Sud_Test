8f63ce1da41a353510b23b1950f3d634a02f9f28
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 21:56:15 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -14,6 +14,23 @@ hull_uni_coordinates = (53.77114698979646, -0.36430683784066786)
 # Create a DataFrame with a single row containing Hull University coordinates
 df1 = pd.DataFrame({'lat': [hull_uni_coordinates[0]], 'lon': [hull_uni_coordinates[1]]})
 
+  # Load the image
+    image = Image.open('Swale.jpg')
+
+    # Create a layout with two columns
+    col1, col2 = st.columns(2)
+
+    # In the first column, display the map
+    with col1:
+        st.subheader('Map')
+        st.map(df1, zoom=15)
+
+    # In the second column, display the image
+    with col2:
+        st.subheader('Image')
+        st.image(image, caption='Outfall of Swale')
+
+
 # CKAN API endpoint URL
 apiUrl = 'https://opendata.hullcc.gov.uk/api/3'
 
@@ -69,21 +86,7 @@ try:
     )
     st.altair_chart(chart_salinity, use_container_width=True)
 
-    # Load the image
-    image = Image.open('Swale.jpg')
-
-    # Create a layout with two columns
-    col1, col2 = st.columns(2)
-
-    # In the first column, display the map
-    with col1:
-        st.subheader('Map')
-        st.map(df1, zoom=15)
-
-    # In the second column, display the image
-    with col2:
-        st.subheader('Image')
-        st.image(image, caption='Outfall of Swale')
+  
 
 except:
     st.write('Error occurred while retrieving the dataset.')
