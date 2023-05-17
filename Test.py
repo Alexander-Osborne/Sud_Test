4b6bdc77ad93e0d40009124878f497d4ee8db455
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 13:45:57 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,39 +1,7 @@
 import streamlit as st
-import pandas as pd
-import time
-from datetime import datetime, timedelta
 
-# Sample rainfall data
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-rainfall = [45, 62, 32, 14, 67, 78, 92, 85, 47, 32, 15, 23]
+# Latitude and longitude coordinates of Hull University
+hull_university_location = (53.767851, -0.366748)
 
-# Create a dataframe for the data
-df = pd.DataFrame({'Month': months, 'Rainfall (mm)': rainfall})
-
-# Initialize the current time
-now_time = datetime.now()
-
-# Display the initial bar chart using Streamlit
-chart = st.bar_chart(df['Rainfall (mm)'])
-
-# Initialize the time container
-time_container = st.empty()
-
-# Auto-update the bar chart every 10 seconds
-while True:
-    # Update the data
-    df['Rainfall (mm)'] = [value + 5 for value in df['Rainfall (mm)']]
-
-    # Update the chart
-    chart.bar_chart(df['Rainfall (mm)'])
-
-    # Update the current time
-    now_time += timedelta(seconds=10)  # Increment the time by 10 seconds
-    bst_time = now_time + timedelta(minutes=60)  # Adjust the time by adding 60 minutes (1 hour) for BST
-    date_str = bst_time.strftime('%b %d, %Y %H:%M:%S')
-
-    # Display the current time
-    time_container.markdown(f"**Current Time (BST):** {date_str}")
-
-    # Wait for 10 seconds
-    time.sleep(10)
+# Show Hull University on a map
+st.map(hull_university_location)
