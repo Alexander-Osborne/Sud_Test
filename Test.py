4b39a42442b3339a3433744968824a5b18f6bbac
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 21:19:20 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,9 @@
 import streamlit as st
-from PIL import Image
+import pandas as pd
+import numpy as np
 
-image = Image.open('Swale.JPG')
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-st.image(image, caption='Swale')
+st.map(df)
