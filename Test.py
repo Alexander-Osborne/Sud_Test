f2df678cd0206caa14c497cfe521e27bb882444d
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 21:43:36 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,22 +1,86 @@
 import streamlit as st
-from streamlit_folium import folium_static
-import folium
+import requests
+import pandas as pd
+import altair as alt
+from datetime import datetime
+from PIL import Image
 
-"# streamlit-folium"
+# Define the coordinates for Hull University
+hull_uni_coordinates = (53.77114698979646, -0.36430683784066786)
 
-with st.echo():
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import folium
+# Create a DataFrame with a single row containing Hull University coordinates
+df = pd.DataFrame({'lat': [hull_uni_coordinates[0]], 'lon': [hull_uni_coordinates[1]]})
 
-    # center on Liberty Bell
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+# CKAN API endpoint URL
+apiUrl = 'https://opendata.hullcc.gov.uk/api/3'
 
-    # add marker for Liberty Bell
-    tooltip = "Liberty Bell"
-    folium.Marker(
-        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-    ).add_to(m)
+# Dataset resource ID
+resourceId = 'f4f85e47-f8d9-4f13-8138-4bec4afde84d'
 
-    # call to render Folium map in Streamlit
-    folium_static(m)
+# Number of rows to retrieve from the end of the dataset
+numRows = 2000
+
+# Create the API request URL
+apiRequestUrl = f'{apiUrl}/action/datastore_search?resource_id={resourceId}&fields=ts,temp,salinity,depth&sort=_id desc&limit={numRows}'
+
+try:
+    # Send API request to retrieve the dataset
+    response = requests.get(apiRequestUrl)
+    data = response.json()
+    records = data['result']['records']
+
+    # Extract the 'ts' and 'temp' columns from the records
+    tsData = [record['ts'] for record in records]
+    tempData = [record['temp'] for record in records]
+    depthData = [record['depth'] for record in records]
+    salinityData = [record['salinity'] for record in records]
+
+    # Convert timestamps to datetime objects
+    tsData = [datetime.fromtimestamp(ts) for ts in tsData]
+
+    # Create a dataframe for the data
+    df = pd.DataFrame({'Timestamp': tsData, 'Temperature': tempData, 'Depth': depthData, 'Salinity': salinityData})
+
+    # Create three separate figures using Streamlit and Altair
+    st.subheader('Temperature')
+    chart_temp = alt.Chart(df).mark_line().encode(
+        x='Timestamp:T',
+        y='Temperature:Q',
+        color=alt.value('red')
+    )
+    st.altair_chart(chart_temp, use_container_width=True)
+
+    st.subheader('Depth')
+    chart_depth = alt.Chart(df).mark_line().encode(
+        x='Timestamp:T',
+        y='Depth:Q',
+        color=alt.value('blue')
+    )
+    st.altair_chart(chart_depth, use_container_width=True)
+
+    st.subheader('Salinity')
+    chart_salinity = alt.Chart(df).mark_line().encode(
+        x='Timestamp:T',
+        y='Salinity:Q',
+        color=alt.value('green')
+    )
+    st.altair_chart(chart_salinity, use_container_width=True)
+
+    # Load the image
+    image = Image.open('Swale.jpg')
+
+    # Create a layout with two columns
+    col1, col2 = st.columns(2)
+
+    # In the first column, display the map
+    with col1:
+        st.subheader('Map')
+        st.map(df, zoom=15)
+
+    # In the second column, display the image
+    with col2:
+        st.subheader('Image')
+        st.image(image, caption='Swale')
+
+except:
+    st.write('Error occurred while retrieving the dataset.')
