70c0e3953d73ed619534d209d0cbea3f3fbce759
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 21:20:09 +0100

    Update Hull3.py

--- Hull3.py
+++ Hull3.py
@@ -5,6 +5,15 @@ import altair as alt
 from datetime import datetime
 import streamlit as st
 from PIL import Image
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
 
 image = Image.open('Swale.jpg')
 image = image.rotate(90)  # Rotate the image by 90 degrees clockwise
