ebb919b8eff96a1901747ec6c21f897b11f12f23
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-17 13:34:29 +0100

    Update Hull3.py

--- Hull3.py
+++ Hull3.py
@@ -14,25 +14,31 @@ resourceId = '6a9bc5a3-9c5d-4b0c-87af-1a18f7c507ef'
 numRows = 1000
 
 # Create the API request URL
-apiRequestUrl = f'{apiUrl}/action/datastore_search?resource_id={resourceId}&fields=ts,temp,salinity,depth&sort=_id desc&limit={numRows}'
+apiRequestUrl = f'{apiUrl}/action/datastore_search?resource_id={resourceId}&fields=ts,temp,salinity,depth,lat,lng&sort=_id desc&limit={numRows}'
 
 try:
     # Send API request to retrieve the dataset
     response = requests.get(apiRequestUrl)
     data = response.json()
     records = data['result']['records']
-    
+
     # Extract the 'ts' and 'temp' columns from the records
     tsData = [record['ts'] for record in records]
     tempData = [record['temp'] for record in records]
     depthData = [record['depth'] for record in records]
     salinityData = [record['salinity'] for record in records]
+    latData = [record['lat'] for record in records]
+    lngData = [record['lng'] for record in records]
 
     # Convert timestamps to datetime objects
     tsData = [datetime.fromtimestamp(ts) for ts in tsData]
 
     # Create a dataframe for the data
-    df = pd.DataFrame({'Timestamp': tsData, 'Temperature': tempData, 'Depth': depthData, 'Salinity': salinityData})
+    df = pd.DataFrame({'Timestamp': tsData, 'Temperature': tempData, 'Depth': depthData, 'Salinity': salinityData, 'Latitude': latData, 'Longitude': lngData})
+
+    # Create a map at the top
+    st.subheader('Map')
+    st.map(df)
 
     # Create three separate figures using Streamlit and Altair
     st.subheader('Temperature')
