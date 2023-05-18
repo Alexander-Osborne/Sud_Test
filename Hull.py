59af6d09c3cf74d4af7adfa175c651cbfddfe437
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-18 14:39:50 +0100

    Update Hull.py

--- Hull.py
+++ Hull.py
@@ -1,40 +1,14 @@
 import streamlit as st
-import requests
-import pandas as pd
-from datetime import datetime
 
-# CKAN API endpoint URL
-apiUrl = 'https://opendata.hullcc.gov.uk/api/3'
+# Everything is accessible via the st.secrets dict:
 
-# Dataset resource ID
-resourceId = '6a9bc5a3-9c5d-4b0c-87af-1a18f7c507ef'
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
-# Number of rows to retrieve from the end of the dataset
-numRows = 1000
+# And the root-level secrets are also accessible as environment variables:
 
-# Create the API request URL
-apiRequestUrl = f'{apiUrl}/action/datastore_search?resource_id={resourceId}&fields=ts,temp,salinity,depth&sort=_id desc&limit={numRows}'
-
-try:
-    # Send API request to retrieve the dataset
-    response = requests.get(apiRequestUrl)
-    data = response.json()
-    records = data['result']['records']
-    
-    # Extract the 'ts' and 'temp' columns from the records
-    tsData = [record['ts'] for record in records]
-    tempData = [record['temp'] for record in records]
-    depthData = [record['depth'] for record in records]
-    salinityData = [record['salinity'] for record in records]
-
-    # Convert timestamps to datetime objects
-    tsData = [datetime.fromtimestamp(ts) for ts in tsData]
-
-    # Create a dataframe for the data
-    df = pd.DataFrame({'Timestamp': tsData, 'Temperature': tempData, 'Depth': depthData, 'Salinity': salinityData})
-
-    # Display the line charts using Streamlit
-    st.line_chart(df.set_index('Timestamp'))
-
-except:
-    st.write('Error occurred while retrieving the dataset.')
+import os
+st.write(
+	"Has environment variables been set:",
+	os.environ["db_username"] == st.secrets["db_username"])
