b17807d49c6d2ec17a7010e6a7942d2a6900cc40
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 16:44:56 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,61 +1,50 @@
 import streamlit as st
 import folium
 import pandas as pd
-from folium import features
-from streamlit_folium import folium_static
+from streamlit_folium import st_folium
 import numpy as np
 
 def create_map():
-    # Create the map object with the CartoDB Positron tileset and set the initial zoom location to England
-    map = folium.Map(location=[53.0, -1.0], zoom_start=6, tiles='CartoDB Positron')
+    m = folium.Map(location=[39.8283, -98.5795], zoom_start=5)
+    fg = folium.FeatureGroup(name="State bounds")
+    fg.add_child(folium.features.GeoJson(bounds))
 
-    # Read marker information from the CSV file
-    markers_df = pd.read_csv("markers.csv")
+    capitals = STATE_DATA
 
-    # Create markers for each row in the CSV with custom icons
-    for index, row in markers_df.iterrows():
-        # Create a custom marker icon with an image
-        icon_image = row["marker_icon"]  # Column name in the CSV for marker icon image path
-        icon = features.CustomIcon(icon_image, icon_size=(30, 30))
+    for capital in capitals.itertuples():
+        fg.add_child(
+            folium.Marker(
+                location=[capital.latitude, capital.longitude],
+                popup=f"{capital.capital}, {capital.state}",
+                tooltip=f"{capital.capital}, {capital.state}",
+                icon=folium.Icon(color="green")
+                if capital.state == st.session_state.get("selected_state")
+                else None,
+            )
+        )
 
-        # Add the marker to the map
-        marker = folium.Marker(
-            location=[row["latitude"], row["longitude"]],
-            popup=row["name"],
-            tooltip=row["name"],
-            icon=icon
-        ).add_to(map)
-
-        # Create a custom JavaScript snippet to handle the marker click event and set the selected_marker value
-        js_code = f"""
-            function onMarkerClick(e) {{
-                var markerIndex = {index};
-                Streamlit.setComponentValue('selected_marker', markerIndex);
-            }}
-            var marker = arguments[0];
-            marker.on('click', onMarkerClick);
-        """
-
-        # Add the JavaScript code to the HTML template using Streamlit's components API
-        st.components.v1.html(f'<script>{js_code}</script>')
-
-    return map
+    m.add_child(fg)
+    return m
 
 def main():
-    st.title("Monitoring Sites Map")
-    st.markdown("Map showing the location of monitoring sites")
+    st.title("State Capitals Map")
+    st.markdown("Map showing state capitals")
 
-    map = create_map()
-    folium_static(map)
+    m = create_map()
+    out = st_folium(
+        m,
+        width=1200,
+        height=500,
+    )
 
-    selected_marker = st.session_state.get("selected_marker")
+    selected_state = st.session_state.get("selected_state")
 
     # Display graph below the map when a marker is clicked
-    if selected_marker is not None:
-        st.write(f"You clicked marker {selected_marker}. Here is additional content.")
-        
+    if selected_state is not None:
+        st.write(f"You clicked state: {selected_state}. Here is additional content.")
+
         # Generate example data for the line chart
-        np.random.seed(selected_marker)
+        np.random.seed(hash(selected_state) % 100)
         data = np.random.randn(100).cumsum()
 
         # Display the line chart below the map
