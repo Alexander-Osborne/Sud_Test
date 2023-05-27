9cf482770e9fd4d9f4102f02435d72a0c6c2ae29
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:12:06 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,7 +2,6 @@ import streamlit as st
 import folium
 from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
-from streamlit_folium import CustomJSElement
 
 # Add a title to your app
 st.title("Map App")
@@ -16,30 +15,21 @@ m = folium.Map(location=location, zoom_start=13)
 # Create a marker cluster group
 marker_cluster = MarkerCluster()
 
-# Add a custom JavaScript callback function for click events
-callback = '''
-function(e){
-    var marker = L.marker(e.latlng)
-        .bindPopup("Marker")
-        .addTo(markerCluster);
-}
+# Add markers to the map
+marker1 = folium.Marker([51.5074, -0.1278], popup='Marker 1')
+marker2 = folium.Marker([51.5072, -0.1276], popup='Marker 2')
 
-map.on("click", callback);
-'''
-
-# Add the JavaScript callback function to the map using CustomJSElement
-CustomJSElement(callback).add_to(m)
+marker_cluster.add_child(marker1)
+marker_cluster.add_child(marker2)
 
 # Add the marker cluster group to the map
 m.add_child(marker_cluster)
 
-# Render the map in Streamlit
-folium_static(m)
+# Render the map in Streamlit using st.map()
+st.map(m)
 
 # Get the selected marker's coordinates
-selected_marker = st.selectbox("Select a marker", [marker.get_popup().replace('\n', '') for marker in marker_cluster._children])
+selected_marker = st.selectbox("Select a marker", ["Marker 1", "Marker 2"])
 
-if selected_marker:
-    st.write("You selected the marker with coordinates:")
-    st.write(selected_marker)
-    # Add your Streamlit code for the selected marker
+if selected_marker == "Marker 1":
+    st.write("You selected Marker 1. Add your Streamlit code here.")
