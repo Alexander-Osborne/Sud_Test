40a063ad3feda13894a303f2609a89c3a5b3158a
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:23:44 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 # Create a Streamlit app and set a title
 st.title("Map App")
@@ -18,25 +18,27 @@ data = {
 }
 df = pd.DataFrame(data)
 
-# Create a scatter_geo plot
-fig = px.scatter_geo(df, lat='Latitude', lon='Longitude', hover_name='Marker')
+# Create a scattermapbox plot
+fig = go.Figure(go.Scattermapbox(
+    lat=df['Latitude'],
+    lon=df['Longitude'],
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=10,
+        color='blue'
+    ),
+    text=df['Marker']
+))
 
-# Update the layout for the map
-fig.update_geos(
-    center=dict(lat=latitude, lon=longitude),
-    projection_type="natural earth",
-    showcountries=True,
-    countrycolor="gray",
-    showland=True,
-    landcolor="lightgray",
-    showocean=True,
-    oceancolor="lightblue",
-    showrivers=True,
-    rivercolor="blue",
+# Set the mapbox style and center the map
+fig.update_layout(
+    mapbox=dict(
+        style='carto-positron',
+        center=dict(lat=latitude, lon=longitude),
+        zoom=zoom_level
+    ),
+    margin=dict(l=0, r=0, t=0, b=0)
 )
-
-# Set the map zoom level
-fig.update_geos(fitbounds="locations", visible=False)
 
 # Render the map in Streamlit
 st.plotly_chart(fig)
