fa1cade8559c21d0c54f740430918388ee7a1ca1
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:37:42 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -11,31 +11,47 @@ latitude = 51.5074  # London, UK
 longitude = -0.1278
 zoom_level = 10
 
-# Create a sample dataframe with marker data
+# Create a sample dataframe with marker data and types
 data = {
-    'Marker': ['Marker 1', 'Marker 2'],
-    'Latitude': [51.5074, 51.5072],
-    'Longitude': [-0.1278, -0.1276],
+    'Marker': ['Marker 1', 'Marker 2', 'Marker 3'],
+    'Latitude': [51.5074, 51.5072, 51.5080],
+    'Longitude': [-0.1278, -0.1276, -0.1280],
+    'Type': ['swale', 'rain', 'swale']
 }
 df = pd.DataFrame(data)
 
 # Create a folium map
 m = folium.Map(location=[latitude, longitude], zoom_start=zoom_level)
 
-# Add markers to the map
+# Create separate feature groups for different marker types
+swale_group = folium.FeatureGroup(name='Swale')
+rain_group = folium.FeatureGroup(name='Rain')
+
+# Add markers to the respective feature groups based on types
 for i, row in df.iterrows():
-    folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker']).add_to(m)
+    if row['Type'] == 'swale':
+        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker']).add_to(swale_group)
+    elif row['Type'] == 'rain':
+        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker']).add_to(rain_group)
+
+# Add feature groups to the map
+swale_group.add_to(m)
+rain_group.add_to(m)
 
 # Display the map in Streamlit
 folium_static(m)
 
-# Get the selected marker
-selected_marker = st.selectbox("Select a marker", df['Marker'])
+# Get the selected markers
+selected_swales = st.checkbox("Swales", value=True)
+selected_rain = st.checkbox("Rain", value=True)
 
-# Perform actions based on the selected marker
-if selected_marker == 'Marker 1':
-    st.write("You selected Marker 1.")
-    # Add your Streamlit code for Marker 1
-elif selected_marker == 'Marker 2':
-    st.write("You selected Marker 2.")
-    # Add your Streamlit code for Marker 2
+# Perform actions based on the selected markers
+if selected_swales:
+    st.write("Selected Swales:")
+    swale_markers = df.loc[df['Type'] == 'swale', 'Marker']
+    st.write(swale_markers)
+
+if selected_rain:
+    st.write("Selected Rain:")
+    rain_markers = df.loc[df['Type'] == 'rain', 'Marker']
+    st.write(rain_markers)
