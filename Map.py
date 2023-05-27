f310808dc5d3cb8e924cabd7e01e4634d76ad6a6
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 15:53:57 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,31 +2,30 @@ import streamlit as st
 import folium
 from streamlit_folium import folium_static
 
-def main():
-    st.title("Monitoring Sites Map")
-    st.markdown("Map showing the location of monitoring sites")
-
+def create_map():
+    map = folium.Map(location=[37.7749, -122.4194], zoom_start=10)
+    
     site = {
         "name": "Site 1",
         "latitude": 37.7749,
         "longitude": -122.4194,
         "url": "https://example.com/site1"
     }
-
-    map = folium.Map(location=[site["latitude"], site["longitude"]], zoom_start=10)
-
-    marker_html = f'''
-    <h3>{site["name"]}</h3>
-    <a href="{site["url"]}" target="_blank">Go to URL</a>
-    '''
-
+    
     folium.Marker(
         location=[site["latitude"], site["longitude"]],
-        popup=folium.Popup(marker_html, max_width=300),
+        popup=f'<a href="{site["url"]}" target="_blank">{site["name"]}</a>',
         tooltip=site["name"]
     ).add_to(map)
+    
+    return map
 
-    st_folium(map)
+def main():
+    st.title("Monitoring Sites Map")
+    st.markdown("Map showing the location of monitoring sites")
+
+    map = create_map()
+    folium_static(map)
 
 if __name__ == "__main__":
     main()
