dcf9d2223ffb9f76b71dfaca1207b414a355ca8b
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 21:03:21 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -25,6 +25,41 @@ def main():
         render_blank_page()
 
 def render_map_page():
+    st.title("Map Page")
+
+    # Create a map object with CartoDB Positron basemap
+    m = folium.Map(location=[51.5074, -0.1278], zoom_start=12, tiles="CartoDB Positron")
+
+    # Generate random markers with classifications
+    markers = []
+    classes = ["Class A", "Class B", "Class C"]
+
+    # Create marker clusters for each class
+    marker_clusters = {classification: MarkerCluster(name=classification) for classification in classes}
+
+    for _ in range(10):
+        latitude = random.uniform(51.4, 51.6)
+        longitude = random.uniform(-0.2, 0.2)
+        classification = random.choice(classes)
+
+        marker = folium.Marker(location=[latitude, longitude], popup='Random Location', tooltip=classification)
+
+        # Add marker to the corresponding marker cluster
+        marker.add_to(marker_clusters[classification])
+
+        markers.append(marker)
+
+    # Add marker clusters to the map
+    for marker_cluster in marker_clusters.values():
+        marker_cluster.add_to(m)
+
+    # Add layer control to toggle marker clusters
+    folium.LayerControl().add_to(m)
+
+    # Render the map
+    folium_static(m)
+
+def render_blank_page():
     st.title('SuDS_lab_UK - Wilberforce 001')
 
     # Define the coordinates for Hull University
@@ -155,10 +190,6 @@ def render_map_page():
     b64 = base64.b64encode(csv_data.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="SuDSlab_Data.csv">Download SuDSlab Data</a>'
     st.markdown(href, unsafe_allow_html=True)
-
-def render_blank_page():
-    st.title("Blank Page")
-    # Your content for the blank page goes here
 
 if __name__ == "__main__":
     main()
