481478dc88095335b63b81101dfd38ca0adc1267
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:43:07 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import folium
 from streamlit_folium import folium_static
 
@@ -23,20 +22,12 @@ df = pd.DataFrame(data)
 # Create a folium map
 m = folium.Map(location=[latitude, longitude], zoom_start=zoom_level)
 
-# Create separate feature groups for different marker types
-swale_group = folium.FeatureGroup(name='Swale')
-rain_group = folium.FeatureGroup(name='Rain')
-
-# Add markers to the respective feature groups based on types
-for i, row in df.iterrows():
+# Iterate through the dataframe and add markers based on types
+for _, row in df.iterrows():
     if row['Type'] == 'swale':
-        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker']).add_to(swale_group)
+        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker'], icon=folium.Icon(color='blue')).add_to(m)
     elif row['Type'] == 'rain':
-        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker']).add_to(rain_group)
-
-# Add feature groups to the map
-swale_group.add_to(m)
-rain_group.add_to(m)
+        folium.Marker([row['Latitude'], row['Longitude']], popup=row['Marker'], icon=folium.Icon(color='green')).add_to(m)
 
 # Display the map in Streamlit
 folium_static(m)
@@ -45,11 +36,8 @@ folium_static(m)
 selected_swales = st.checkbox("Swales", value=True)
 selected_rain = st.checkbox("Rain", value=True)
 
-# Add or remove marker groups based on selected checkboxes
+# Update the map based on the selected checkboxes
 if not selected_swales:
-    if swale_group.get_name() in m._children:
-        m.remove_child(swale_group)
-
-if not selected_rain:
-    if rain_group.get_name() in m._children:
-        m.remove_child(rain_group)
+    for child in m.get_root().children:
+        if child.layer_name == 'Swale':
+           
