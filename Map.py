4694e12a092ff34ad2a3533ec69a2bbe5c3e24ed
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:16:13 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,30 +1,47 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, TapTool, OpenURL
+from bokeh.tile_providers import get_provider, Vendors
+from streamlit.bokeh_events import streamlit_bokeh_events
 
-# Create a Streamlit app and set a title
-st.title("Map App")
+# Define the map location and zoom level
+location = (51.5074, -0.1278)  # London, UK
+zoom_level = 12
 
-# Create a Folium map object
-m = folium.Map(location=[51.5074, -0.1278], zoom_start=13)
+# Create a Bokeh figure and set the tile provider
+tile_provider = get_provider(Vendors.CARTODBPOSITRON_RETINA)
+p = figure(x_range=(-200000, 2000000), y_range=(-200000, 7000000),
+           x_axis_type="mercator", y_axis_type="mercator")
+p.add_tile(tile_provider)
+
+# Create a ColumnDataSource to store marker data
+source = ColumnDataSource(data=dict(lat=[], lon=[], names=[]))
 
 # Add clickable markers to the map
-marker1 = folium.Marker([51.5074, -0.1278], popup='Marker 1')
-marker2 = folium.Marker([51.5072, -0.1276], popup='Marker 2')
+p.circle(x='lon', y='lat', size=10, source=source, alpha=0.7)
 
-marker1.add_to(m)
-marker2.add_to(m)
+# Add a TapTool to handle click events on markers
+taptool = p.select(type=TapTool)
+taptool.callback = OpenURL(url="@names")
 
-# Render the map in Streamlit
-folium_static(m)
+# Define a Streamlit app and set the title
+st.title("Map App")
 
-# Add a selectbox to select a marker
-selected_marker = st.selectbox("Select a marker", ["Marker 1", "Marker 2"])
+# Render the Bokeh figure using streamlit.bokeh_events
+bokeh_event_data = streamlit_bokeh_events(figure=p, events="tap", key="map")
 
-# Add conditional statements based on the selected marker
-if selected_marker == "Marker 1":
-    st.write("You selected Marker 1. Add your Streamlit code here.")
-    # Add your Streamlit code for Marker 1
-elif selected_marker == "Marker 2":
-    st.write("You selected Marker 2. Add your Streamlit code here.")
-    # Add your Streamlit code for Marker 2
+# Handle the click event and update the marker data
+if bokeh_event_data:
+    if "map_tap" in bokeh_event_data:
+        lon, lat = bokeh_event_data["map_tap"]["x"], bokeh_event_data["map_tap"]["y"]
+        source.data = dict(lat=[lat], lon=[lon], names=["Marker"])
+
+# Display the map in Streamlit
+st.bokeh_chart(p)
+
+# Get the selected marker's coordinates
+selected_marker = st.selectbox("Select a marker", ["Marker"])
+
+if selected_marker:
+    st.write("You selected the marker:", selected_marker)
+    # Add your Streamlit code for the selected marker
