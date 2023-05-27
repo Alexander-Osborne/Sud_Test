d439143f2a0d28ca1b29cddd520b6e14e0e0bbdb
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 17:43:03 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -14,13 +14,59 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-# Retrieve secrets from Streamlit Secrets
+
+
+def create_map():
+    # Create the map object with the CartoDB Positron tileset and set the initial zoom location to England
+    map = folium.Map(location=[53.0, -1.0], zoom_start=6, tiles='CartoDB Positron')
+
+    # Read marker information from the CSV file
+    markers_df = pd.read_csv("markers.csv")
+
+    # Create markers for each row in the CSV with custom icons
+    for index, row in markers_df.iterrows():
+        # Create a custom marker icon with an image
+        icon_image = row["marker_icon"]  # Column name in the CSV for marker icon image path
+        icon = features.CustomIcon(icon_image, icon_size=(30, 30))
+
+        # Add the marker to the map
+        marker = folium.Marker(
+            location=[row["latitude"], row["longitude"]],
+            popup=row["name"],
+            tooltip=row["name"],
+            icon=icon
+        ).add_to(map)
+
+        # Add the marker index as a button on the sidebar
+        button = st.sidebar.button(f"Marker {index + 1}")
+
+        # Set the selected_marker value when the button is clicked
+        if button:
+            st.session_state.selected_marker = index
+
+    return map
+
+def main():
+    st.title("Monitoring Sites Map")
+    st.markdown("Map showing the location of monitoring sites")
+
+    map = create_map()
+    folium_static(map)
+
+    selected_marker = st.session_state.get("selected_marker")
+
+    # Display graph below the map when a marker is clicked
+    if selected_marker is not None:
+        st.write(f"You clicked marker {selected_marker + 1}. Here is additional content.")
+
+        # Generate example data for the line chart
+        # Retrieve secrets from Streamlit Secrets
 secret_key = st.secrets["secret_key"]
 api_key = st.secrets["api_key"]
 station_id = st.secrets["station_id"]
 
 # Define the number of days to retrieve data for
-num_days = 14
+num_days = 1
 
 # Initialize an empty list to store the data frames for each day
 data_frames = []
@@ -113,51 +159,6 @@ for i in range(num_days):
 # Concatenate all the data frames into a single data frame
 combined_df = pd.concat(data_frames)
 
-
-def create_map():
-    # Create the map object with the CartoDB Positron tileset and set the initial zoom location to England
-    map = folium.Map(location=[53.0, -1.0], zoom_start=6, tiles='CartoDB Positron')
-
-    # Read marker information from the CSV file
-    markers_df = pd.read_csv("markers.csv")
-
-    # Create markers for each row in the CSV with custom icons
-    for index, row in markers_df.iterrows():
-        # Create a custom marker icon with an image
-        icon_image = row["marker_icon"]  # Column name in the CSV for marker icon image path
-        icon = features.CustomIcon(icon_image, icon_size=(30, 30))
-
-        # Add the marker to the map
-        marker = folium.Marker(
-            location=[row["latitude"], row["longitude"]],
-            popup=row["name"],
-            tooltip=row["name"],
-            icon=icon
-        ).add_to(map)
-
-        # Add the marker index as a button on the sidebar
-        button = st.sidebar.button(f"Marker {index + 1}")
-
-        # Set the selected_marker value when the button is clicked
-        if button:
-            st.session_state.selected_marker = index
-
-    return map
-
-def main():
-    st.title("Monitoring Sites Map")
-    st.markdown("Map showing the location of monitoring sites")
-
-    map = create_map()
-    folium_static(map)
-
-    selected_marker = st.session_state.get("selected_marker")
-
-    # Display graph below the map when a marker is clicked
-    if selected_marker is not None:
-        st.write(f"You clicked marker {selected_marker + 1}. Here is additional content.")
-
-        # Generate example data for the line chart
         # Display the line chart for 'depth'
         st.line_chart(depth_data.rename(columns={'ts': 'DateTime', 'depth': 'Depth (m)'}).set_index('DateTime'))
                      
