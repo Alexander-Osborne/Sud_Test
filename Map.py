be892f2955f2960dd9717e5582525c435553fb85
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:12:57 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,35 +1,30 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
 
-# Add a title to your app
+# Create a Streamlit app and set a title
 st.title("Map App")
 
-# Define the map location
-location = [51.5074, -0.1278]  # London, UK
+# Create a Folium map object
+m = folium.Map(location=[51.5074, -0.1278], zoom_start=13)
 
-# Create a folium map object
-m = folium.Map(location=location, zoom_start=13)
-
-# Create a marker cluster group
-marker_cluster = MarkerCluster()
-
-# Add markers to the map
+# Add clickable markers to the map
 marker1 = folium.Marker([51.5074, -0.1278], popup='Marker 1')
 marker2 = folium.Marker([51.5072, -0.1276], popup='Marker 2')
 
-marker_cluster.add_child(marker1)
-marker_cluster.add_child(marker2)
+marker1.add_to(m)
+marker2.add_to(m)
 
-# Add the marker cluster group to the map
-m.add_child(marker_cluster)
+# Render the map in Streamlit
+folium_static(m)
 
-# Render the map in Streamlit using st.map()
-st.map(m)
-
-# Get the selected marker's coordinates
+# Add a selectbox to select a marker
 selected_marker = st.selectbox("Select a marker", ["Marker 1", "Marker 2"])
 
+# Add conditional statements based on the selected marker
 if selected_marker == "Marker 1":
     st.write("You selected Marker 1. Add your Streamlit code here.")
+    # Add your Streamlit code for Marker 1
+elif selected_marker == "Marker 2":
+    st.write("You selected Marker 2. Add your Streamlit code here.")
+    # Add your Streamlit code for Marker 2
