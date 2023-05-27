b4b60e0a0ce84b70e75f59cb5b6d45bad013cc4f
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 23:58:43 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -220,6 +220,11 @@ def render_blank_page():
             chart_data = combined_df[['ts'] + moist_soil_columns].rename(columns={'ts': 'DateTime'})
             st.line_chart(chart_data.set_index('DateTime'))
 
+        # Check if any of the 'moist_soil_last' columns exist in the combined DataFrame
+        salinity_columns = [col for col in combined_df.columns if col.startswith('salinity_last_')]
+        if len(salininty_columns) > 0:
+            chart_data = combined_df[['ts'] + salininty_columns].rename(columns={'ts': 'DateTime'})
+            st.line_chart(chart_data.set_index('DateTime'))
 
             
             
