bfd993d94a8ed24c96aa9a276d1e9cf54583af52
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 18:06:23 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster, ClickForMarker
 
 # Add a title to your app
 st.title("Map App")
@@ -11,23 +12,30 @@ location = [51.5074, -0.1278]  # London, UK
 # Create a folium map object
 m = folium.Map(location=location, zoom_start=13)
 
-# Add markers to the map
-marker1 = folium.Marker([51.5074, -0.1278], popup='Marker 1')
-marker2 = folium.Marker([51.5072, -0.1276], popup='Marker 2')
+# Add the ClickForMarker plugin to the map
+click_marker = ClickForMarker(popup='Clicked Marker')
+m.add_child(click_marker)
 
-# Add the markers to the map
-m.add_child(marker1)
-m.add_child(marker2)
+# Create a marker cluster group
+marker_cluster = MarkerCluster()
+
+# Register the click event handler
+def on_click(e):
+    lat, lon = e.latlng
+    marker = folium.Marker([lat, lon], popup='Marker')
+    marker_cluster.add_child(marker)
+
+m.add_child(marker_cluster)
+m.add_child(folium.LatLngPopup())
 
 # Render the map in Streamlit
 folium_static(m)
 
-# Add a sub-window for the selected marker
-selected_marker = st.selectbox("Select a marker", ["Marker 1", "Marker 2"])
+# Get the selected marker's coordinates
+selected_marker = st.selectbox("Select a marker", marker_cluster._children.keys(), format_func=lambda x: x.get_name() if x else None)
 
-if selected_marker == "Marker 1":
-    st.write("You selected Marker 1. Add your Streamlit code here.")
-    # Add your Streamlit code for Marker 1
-elif selected_marker == "Marker 2":
-    st.write("You selected Marker 2. Add your Streamlit code here.")
-    # Add your Streamlit code for Marker 2
+if selected_marker:
+    st.write("You selected the marker with coordinates:")
+    st.write(f"Latitude: {selected_marker.location[0]}")
+    st.write(f"Longitude: {selected_marker.location[1]}")
+    # Add your Streamlit code for the selected marker
