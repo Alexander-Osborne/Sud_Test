4ecccfccf769d056cdf234a0991d2378e5df97f4
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-05-27 17:47:32 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -60,116 +60,116 @@ def main():
         st.write(f"You clicked marker {selected_marker + 1}. Here is additional content.")
 
         # Generate example data for the line chart
-# Retrieve secrets from Streamlit Secrets
-secret_key = st.secrets["secret_key"]
-api_key = st.secrets["api_key"]
-station_id = st.secrets["station_id"]
+        # Retrieve secrets from Streamlit Secrets
+        secret_key = st.secrets["secret_key"]
+        api_key = st.secrets["api_key"]
+        station_id = st.secrets["station_id"]
 
-# Define the number of days to retrieve data for
-num_days = 14
+        # Define the number of days to retrieve data for
+        num_days = 14
 
-# Initialize an empty list to store the data frames for each day
-data_frames = []
+        # Initialize an empty list to store the data frames for each day
+        data_frames = []
 
-# Loop over the past 'num_days' to retrieve data for each day
-for i in range(num_days):
-    # Calculate the start and end timestamps for the current day
-    start_timestamp = str(int(time.time()) - (i + 1) * 86400)
-    end_timestamp = str(int(time.time()) - i * 86400)
+        # Loop over the past 'num_days' to retrieve data for each day
+        for i in range(num_days):
+         # Calculate the start and end timestamps for the current day
+         start_timestamp = str(int(time.time()) - (i + 1) * 86400)
+         end_timestamp = str(int(time.time()) - i * 86400)
 
-    # Step 1: Sort parameters by parameter name
-    params = {
-        "api-key": api_key,
-        "end-timestamp": end_timestamp,
-        "start-timestamp": start_timestamp,
-        "station-id": station_id,
-        "t": str(int(time.time()))
-    }
-    sorted_params = sorted(params.items(), key=lambda x: x[0])
+            # Step 1: Sort parameters by parameter name
+            params = {
+              "api-key": api_key,
+             "end-timestamp": end_timestamp,
+             "start-timestamp": start_timestamp,
+              "station-id": station_id,
+              "t": str(int(time.time()))
+         }
+         sorted_params = sorted(params.items(), key=lambda x: x[0])
 
-    # Step 2: Create concatenated string
-    concatenated_string = "".join([f"{param}{value}" for param, value in sorted_params])
+         # Step 2: Create concatenated string
+         concatenated_string = "".join([f"{param}{value}" for param, value in sorted_params])
 
-    # Step 3: Compute HMAC API Signature
-    message = concatenated_string.encode()
-    secret_key_bytes = secret_key.encode()
-    hmac_signature = hmac.new(secret_key_bytes, message, hashlib.sha256).hexdigest()
+         # Step 3: Compute HMAC API Signature
+         message = concatenated_string.encode()
+         secret_key_bytes = secret_key.encode()
+         hmac_signature = hmac.new(secret_key_bytes, message, hashlib.sha256).hexdigest()
 
-    # Step 4: Generate API URL
-    base_url = "https://api.weatherlink.com/v2/historic/"
-    query_params = {
-        "api-key": api_key,
-        "t": str(int(time.time())),
-        "start-timestamp": start_timestamp,
-        "end-timestamp": end_timestamp
-    }
-    query_string = urlencode(query_params)
-    url = f"{base_url}{station_id}?{query_string}&api-signature={hmac_signature}"
+         # Step 4: Generate API URL
+         base_url = "https://api.weatherlink.com/v2/historic/"
+         query_params = {
+             "api-key": api_key,
+             "t": str(int(time.time())),
+             "start-timestamp": start_timestamp,
+             "end-timestamp": end_timestamp
+          }
+         query_string = urlencode(query_params)
+         url = f"{base_url}{station_id}?{query_string}&api-signature={hmac_signature}"
 
-    # Step 5: Load JSON data from the API URL
-    response = requests.get(url)
-    json_data = response.json()
+         # Step 5: Load JSON data from the API URL
+         response = requests.get(url)
+         json_data = response.json()
 
-    # Function to filter the JSON tree based on "lsid"
-    def filter_tree(data, lsid):
-        if isinstance(data, dict):
-            tree = {}
-            if "lsid" in data and data["lsid"] == lsid:
-                return data
-            else:
-                for key, value in data.items():
-                    subtree = filter_tree(value, lsid)
-                    if subtree:
-                        tree[key] = subtree
-                return tree
-        elif isinstance(data, list):
-            tree = []
-            for item in data:
-                subtree = filter_tree(item, lsid)
-                if subtree:
-                    tree.append(subtree)
-            return tree
+          # Function to filter the JSON tree based on "lsid"
+          def filter_tree(data, lsid):
+             if isinstance(data, dict):
+                    tree = {}
+                    if "lsid" in data and data["lsid"] == lsid:
+                       return data
+                   else:
+                        for key, value in data.items():
+                           subtree = filter_tree(value, lsid)
+                           if subtree:
+                               tree[key] = subtree
+                        return tree
+              elif isinstance(data, list):
+                  tree = []
+                 for item in data:
+                        subtree = filter_tree(item, lsid)
+                       if subtree:
+                           tree.append(subtree)
+                    return tree
 
-    # Convert the JSON data into a tree
-    tree = json_data
+          # Convert the JSON data into a tree
+          tree = json_data
 
-    # Filter the JSON tree based on "lsid" equal to 459397
-    lsid_to_filter = 478072
-    filtered_tree = filter_tree(tree, lsid_to_filter)
+           # Filter the JSON tree based on "lsid" equal to 459397
+          lsid_to_filter = 478072
+          filtered_tree = filter_tree(tree, lsid_to_filter)
 
-    # Extract the relevant information from the JSON
-    sensor_data = filtered_tree['sensors'][0]['data']
+          # Extract the relevant information from the JSON
+          sensor_data = filtered_tree['sensors'][0]['data']
 
-    # Convert the data into a DataFrame
-    df = pd.json_normalize(sensor_data)
+           # Convert the data into a DataFrame
+           df = pd.json_normalize(sensor_data)
 
-    # Convert 'depth' from feet to meters
-    df['depth'] = df['depth'] * 0.3048
+          # Convert 'depth' from feet to meters
+          df['depth'] = df['depth'] * 0.3048
 
-    # Convert 'ts' from Unix timestamp to datetime
-    df['ts'] = pd.to_datetime(df['ts'], unit='s')
+           # Convert 'ts' from Unix timestamp to datetime
+           df['ts'] = pd.to_datetime(df['ts'], unit='s')
 
-    df['salinity'] = df['salinity']
+           df['salinity'] = df['salinity']
 
-    df['temp'] = (df['temp'] - 32) * 5 / 9
+           df['temp'] = (df['temp'] - 32) * 5 / 9
 
-    # Append the extracted data to the list
-    data_frames.append(df)
+            # Append the extracted data to the list
+            data_frames.append(df)
 
-# Concatenate all the data frames into a single data frame
-combined_df = pd.concat(data_frames)
+        # Concatenate all the data frames into a single data frame
+        combined_df = pd.concat(data_frames)
 
-# Display the line chart for 'depth'
-st.line_chart(combined_df[['ts', 'depth']].rename(columns={'ts': 'DateTime', 'depth': 'Depth (m)'}).set_index('DateTime'))
+        # Display the line chart for 'depth'
+        st.line_chart(combined_df[['ts', 'depth']].rename(columns={'ts': 'DateTime', 'depth': 'Depth (m)'}).set_index('DateTime'))
 
-# Display the line chart for 'temperature'
-st.line_chart(combined_df[['ts', 'temp']].rename(columns={'ts': 'DateTime', 'temp': 'Temperature (\u00B0C)'}).set_index('DateTime'))
+        # Display the line chart for 'temperature'
+        st.line_chart(combined_df[['ts', 'temp']].rename(columns={'ts': 'DateTime', 'temp': 'Temperature (\u00B0C)'}).set_index('DateTime'))
 
-# Display the line chart for 'salinity'
-st.line_chart(combined_df[['ts', 'salinity']].rename(columns={'ts': 'DateTime', 'salinity': 'Salinity'}).set_index('DateTime'))
+        # Display the line chart for 'salinity'
+        st.line_chart(combined_df[['ts', 'salinity']].rename(columns={'ts': 'DateTime', 'salinity': 'Salinity'}).set_index('DateTime'))
 
-# Download button for CSV file
-csv_data = combined_df.to_csv(index=False)
-b64 = base64.b64encode(csv_data.encode()).decode()
+        # Download button for CSV file
+        csv_data = combined_df.to_csv(index=False)
+        b64 = base64.b64encode(csv_data.encode()).decode()
 if __name__ == "__main__":
     main()
