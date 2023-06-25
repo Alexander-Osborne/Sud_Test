1354da534016f19bc4ac79e7ad03e9ef31f93817
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-06-25 14:22:44 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,30 +1,52 @@
+import streamlit as st
 import folium
+from streamlit_folium import folium_static
 import altair as alt
 import pandas as pd
 
-# Create a sample DataFrame
-data = pd.DataFrame({
-    'category': ['A', 'B', 'C', 'D'],
-    'value': [10, 20, 15, 12]
-})
+def render_map_page():
+    st.markdown('<h1 style="text-align: center;">Location Viewer with Vega Popups</h1>', unsafe_allow_html=True)
 
-# Create the Vega chart
-chart = alt.Chart(data).mark_bar().encode(
-    x='category',
-    y='value'
-)
+    # Create a sample DataFrame
+    data = pd.DataFrame({
+        'Location': ['Location 1', 'Location 2', 'Location 3'],
+        'Value': [10, 20, 15]
+    })
 
-# Create the Folium map
-m = folium.Map(location=[53.7701, -0.3672], zoom_start=9)
+    # Create a Vega bar chart
+    chart = alt.Chart(data).mark_bar().encode(
+        x='Location',
+        y='Value'
+    ).properties(
+        width=400,
+        height=300
+    )
 
-# Create a marker with a popup containing the Vega chart
-marker = folium.Marker(
-    location=[53.7701, -0.3672],
-    popup=folium.Popup(chart.to_html(), max_width=400)
-)
+    # Create the Folium map
+    m = folium.Map(location=[53.7701, -0.3672], zoom_start=9)
 
-# Add the marker to the map
-marker.add_to(m)
+    # Add markers to the map with Vega popups
+    for _, row in data.iterrows():
+        location = row['Location']
+        value = row['Value']
 
-# Render the map
-folium_static(m)
+        # Create the Vega chart HTML
+        chart_html = chart.to_html()
+
+        # Create the Folium popup with the Vega chart
+        popup = folium.Popup(chart_html, max_width=600)
+
+        # Add the marker with the popup to the map
+        folium.Marker(
+            location=[53.7701, -0.3672],  # Use your actual coordinates
+            popup=popup
+        ).add_to(m)
+
+    # Render the map
+    folium_static(m)
+
+def main():
+    render_map_page()
+
+if __name__ == "__main__":
+    main()
