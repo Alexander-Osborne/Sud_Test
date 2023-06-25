6e94d08355ba75ea75df8b368319a46c80845670
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-06-25 15:45:31 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,39 +4,73 @@ import streamlit.components.v1 as components
 # bootstrap 4 collapse example
 components.html(
     """
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <div id="accordion">
-      <div class="card">
-        <div class="card-header" id="headingOne">
-          <h5 class="mb-0">
-            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            Collapsible Group Item #1
-            </button>
-          </h5>
-        </div>
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-          <div class="card-body">
-            Collapsible Group Item #1 content
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-header" id="headingTwo">
-          <h5 class="mb-0">
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Collapsible Group Item #2
-            </button>
-          </h5>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-          <div class="card-body">
-            Collapsible Group Item #2 content
-          </div>
-        </div>
-      </div>
-    </div>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>UK Map with Markers</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
+  <style>
+    #map { height: 400px; }
+  </style>
+</head>
+<body>
+  <div id="map"></div>
+
+  <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/papaparse@5.3.0"></script>
+  <script>
+    var map = L.map('map').setView([53.771552, -0.36425564], 15); // Set initial view to the first marker
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+
+    // Fetch the CSV file
+    fetch('https://raw.githubusercontent.com/Alexander-Osborne/Sud_Test/main/markers.csv')
+      .then(response => response.text())
+      .then(data => {
+        // Parse the CSV data
+        var parsedData = Papa.parse(data, { header: true }).data;
+
+        // Define the base URL for marker icons
+        var iconBaseUrl = 'https://raw.githubusercontent.com/Alexander-Osborne/Sud_Test/main/marker_icons/';
+
+        // Iterate through the parsed data and add markers to the map
+        parsedData.forEach(row => {
+          var lat = parseFloat(row.latitude);
+          var lng = parseFloat(row.longitude);
+
+          if (!isNaN(lat) && !isNaN(lng)) {
+            var iconUrl = iconBaseUrl + row.marker_icons.trim();
+            var markerIcon = L.icon({
+              iconUrl: iconUrl,
+              iconSize: [32, 32],
+              iconAnchor: [16, 32],
+              popupAnchor: [0, -32]
+            });
+
+            var popupContent = "<b>" + row.name + "</b><br>" + row.additional_details;
+            var marker = L.marker([lat, lng], { icon: markerIcon }).addTo(map);
+            marker.bindPopup(popupContent).on('popupopen', function(e) {
+              var popup = e.popup;
+              var contentElement = popup.getContent();
+              // Send the popup content to an external destination
+              sendPopupContent(contentElement.innerHTML);
+            });
+          }
+        });
+      });
+
+    function sendPopupContent(content) {
+      // Replace this with your own logic to send the popup content to an external destination
+      // For example, you can use AJAX, fetch, or submit a form to send the content to a server-side script
+      console.log('Popup content:', content);
+    }
+  </script>
+</body>
+</html>
+
     """,
     height=600,
 )
