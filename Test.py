03ca784a4114ae4e4327a1c939b4d021f9a065ac
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-06-25 15:10:33 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,35 +1,39 @@
-import streamlit as st
-from dash import Dash
+import dash
+import dash_core_components as dcc
 import dash_html_components as html
-from dash_extensions.web_components import DashLeafletComponent
-import dash_leaflet as dl
+import plotly.graph_objects as go
 
-# Set Streamlit app title and layout
-st.set_page_config(page_title='University of Hull Map', layout='wide')
+# Create a Dash app
+app = dash.Dash(__name__)
 
-# Streamlit app content
-st.title('University of Hull Map')
-
-# Create a Dash app within Streamlit
-app = Dash(__name__)
-
-# Create a Dash Leaflet map component
-map_component = dl.Map(center=[53.765, -0.335], zoom=15, children=[
-    dl.TileLayer(),
-    dl.Marker(position=[53.765, -0.335], children=[
-        dl.Popup("University of Hull")
-    ])
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("SuDSlab UK", style={"text-align": "center"}),
+    html.P("Data Viewer and Download", style={"text-align": "center"}),
+    dcc.Graph(
+        id="map",
+        figure={
+            "data": [
+                go.Scattermapbox(
+                    lat=[53.77114698979646],
+                    lon=[-0.36430683784066786],
+                    mode="markers",
+                    marker=dict(size=10, color="red"),
+                    text=["Hull University"]
+                )
+            ],
+            "layout": go.Layout(
+                mapbox=dict(
+                    center=dict(lat=53.77114698979646, lon=-0.36430683784066786),
+                    zoom=14
+                ),
+                margin=dict(l=0, r=0, t=30, b=0)
+            )
+        }
+    ),
+    # Add other components or controls as needed
 ])
 
-# Convert the Dash Leaflet map component to a web component
-map_component_webcomponent = DashLeafletComponent(id="map", component_property="children", component_instance=map_component)
-
-# Add the Dash Leaflet map web component to the Dash app layout
-app.layout = html.Div(children=[
-    DashLeafletComponent(),
-    map_component_webcomponent
-])
-
-# Run the Streamlit app with the Dash app embedded
-if __name__ == '__main__':
-    app.run_server(mode='inline')
+# Run the app
+if __name__ == "__main__":
+    app.run_server(debug=True)
