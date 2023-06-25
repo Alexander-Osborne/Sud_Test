b6158807aca6d95c5957bff5410ef7cedd19658b
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-06-25 15:02:37 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dash import Dash
 import dash_html_components as html
-from dash_extensions import webcomponents as wc
+from dash_extensions.web_components import DashLeafletComponent
 import dash_leaflet as dl
 
 # Set Streamlit app title and layout
@@ -22,11 +22,11 @@ map_component = dl.Map(center=[53.765, -0.335], zoom=15, children=[
 ])
 
 # Convert the Dash Leaflet map component to a web component
-map_component_webcomponent = wc.Component(id="map", component_property="children", component_instance=map_component)
+map_component_webcomponent = DashLeafletComponent(id="map", component_property="children", component_instance=map_component)
 
 # Add the Dash Leaflet map web component to the Dash app layout
 app.layout = html.Div(children=[
-    wc.DashLeafletComponent(),
+    DashLeafletComponent(),
     map_component_webcomponent
 ])
 
