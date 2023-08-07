3e2df4907bcde5fe002bdb803057f90bc6c9d606
Alexander-Osborne <102044252+Alexander-Osborne@users.noreply.github.com>
2023-08-07 23:36:07 +0100

    Update Map_Test.py

--- Map_Test.py
+++ Map_Test.py
@@ -11,8 +11,13 @@ random_markers = [
     (-33.8688, 151.2093, "Sydney"),
 ]
 
+# Create a Streamlit session state
+if "selected_marker" not in st.session_state:
+    st.session_state.selected_marker = None
+
 def handle_click(location_name):
-    st.write(f"You clicked on the marker in {location_name}.")
+    st.session_state.selected_marker = location_name
+    st.experimental_rerun()
 
 def main():
     st.title("Random Marker Map")
@@ -23,10 +28,16 @@ def main():
 
     # Add markers to the map
     for lat, lon, location_name in random_markers:
-        folium.Marker(location=[lat, lon], popup=location_name).add_to(m)
+        marker = folium.Marker(location=[lat, lon], popup=location_name, tooltip=location_name)
+        marker.add_to(m)
 
     # Display the map in Streamlit using folium_static
     folium_static(m)
+
+    # Show the selected marker name on a new page
+    if st.session_state.selected_marker:
+        st.title("Selected Marker")
+        st.write(f"You clicked on the marker in {st.session_state.selected_marker}.")
 
 if __name__ == "__main__":
     main()
